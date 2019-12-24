9e85ff262ae8f4dd10c01865d4928f5b5a01260b
chrisrawlings <c.d.rawlings@gmail.com>
2019-12-24 10:29:06 -0500

    User auth working

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user
 import pymysql
 import os
 from flask_mail import Mail, Message
@@ -9,7 +10,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://rawlings:1234@localhost:8889/dnd_char'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://rawlings:1234@localhost:8889/test'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.fatcow.com'
@@ -18,14 +19,15 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'dev@rawlings.site'
 app.config['MAIL_PASSWORD'] = 'Test1Test'
 app.config['MAIL_DEBUG'] = False
-app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_SUPPRESS_SEND'] = True
 app.config['TESTING'] = False
 
 
 db = SQLAlchemy(app)
 mail = Mail(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
+login.login_view = 'login'
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
---
+++ character.py
@@ -0,0 +1,50 @@
+strength = int(10)
+constition = int(10)
+dexterity = int(10)
+wisdom = int(10)
+charisma = int(10)
+pclass = input("what is your class? ")
+
+if pclass == 'Dragonborn':
+    strength = strength + int(2)
+    stats = 'Stats are: ',  strength, dexterity
+    print(stats)
+elif pclass == 'Hill Dwarf':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Mountain Dwarf':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'High Elf':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'wood Elf':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Forest Gnome':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Rock Gnome':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Lighfoot Halfing':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Stout Halfling':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Half Elf':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Half Orc':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Human':
+    strength = num1 + int(2)
+    print(strength)
+elif pclass == 'Tiefling':
+    strength = num1 + int(2)
+    print(strength)
+else:
+    print('that is not a class')
+
